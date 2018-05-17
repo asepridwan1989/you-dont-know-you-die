abce4699de659b8f94db314cc5b405a29b1579a2
asepridwan1989 <aseprmail@gmail.com>
2018-05-17 22:32:14 +0700

    add gameplay

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+       <router-link to="/gameplay">Gameplay</router-link>
     </div>
     <router-view/>
   </div>
@@ -15,11 +16,13 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+ 
 }
 #nav {
   padding: 30px;
 }
-
+body{
+}
 #nav a {
   font-weight: bold;
   color: #2c3e50;
