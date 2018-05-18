733643065a1a046465408679037c9dbc4468b977
anugrah94 <anugrahrezkifauzinasution@gmail.com>
2018-05-18 12:54:20 +0700

    finish register feature

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase'
+const firebaseApp = firebase.initializeApp({
+    apiKey: "AIzaSyANY9ar717wz0zn9wI1YNyjYy-AnrCMxps",
+    authDomain: "ydkyd-3322c.firebaseapp.com",
+    databaseURL: "https://ydkyd-3322c.firebaseio.com",
+    projectId: "ydkyd-3322c",
+    storageBucket: "ydkyd-3322c.appspot.com",
+    messagingSenderId: "986548035683"
+})
+export const db = firebaseApp.database()
--- src/router.js
+++ src/router.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
-import About from './views/About.vue'
+import Register from './views/Register.vue'
 
 Vue.use(Router)
 
@@ -9,13 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: About
+      name: 'register',
+      component: Register
     }
   ]
 })
